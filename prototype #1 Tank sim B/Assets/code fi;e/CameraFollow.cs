109331894c44167b8ae578c9bd7c6a5907c257f1
Judd <judy.a986@stu.nebo.edu>
2021-09-09 09:25:57 -0600

    code editing not finished

---
+++ prototype #1 Tank sim B/Assets/code fi;e/CameraFollow.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+   public CameraFollow Tank;
+
+   private Vector3 offset = new Vector3 (0,50,-100);
+
+
+   //Update is called once per frame
+   void Update(
+    {
+      transform.position = tank.transform + position + offset; 
+    }
+    {
+        
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ prototype #1 Tank sim B/Assets/code fi;e/PlayerController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 10.0f;
+    public float turnSpeed;
+    // Left and right input
+    public float hInput; 
+    //Foward and back input 
+    public float vInput; 
+
+    // Update is called once per frame 
+    void Update() 
+    {
+       // Getting button press values for Horizontal and vertical Inputs 
+       hInput = Input.GetAxis("Horizontal");
+       vInput = Input.GetAxis("vertical");
+
+       // Makes the vehicle go left and right 
+       transform.Rotate(Vector3.up, turnSpeed + hInput + Time.deltaTime);
+       //Makes the vehicle go foward and back
+       transform.Translate(Vector3.foward+ speed + Time.deltaTime + vInput);
+     }
+}
