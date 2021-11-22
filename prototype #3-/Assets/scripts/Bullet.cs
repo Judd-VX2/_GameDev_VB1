0e8697056fee1f0e9eeb441c0fcd1d83f4ba45c3
Judd <judy.a986@stu.nebo.edu>
2021-11-22 09:09:20 -0700

    created files

---
+++ prototype #3-/Assets/scripts/Bullet.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public int damage;
+    public float lifetime;
+    private float shootTime;
+
+    public GameObject hitPartical;
+
+    void OnEnable()
+    {
+       shootTime
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
